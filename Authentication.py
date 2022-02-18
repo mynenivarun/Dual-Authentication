a535193a2293b6f68c94843ec8ccba68a59f111e
VARUN MYNENI <74002498+varun-0007@users.noreply.github.com>
2022-02-18 23:23:35 +0530

    Update Authentication.py

--- Authentication.py
+++ Authentication.py
@@ -1,23 +1,24 @@
 from hashlib import sha512
-print('enter name')
-name = input()
-print('enter password')
-pas = input()
+import getpass
+name = input("Enter Name: ")
+pas = getpass.getpass('Enter the password: ')
 s1=name+pas
 newhash = sha512(s1 .encode()).hexdigest()
-print(newhash)
+print('\n Hash Value \n',newhash,'\n')
 
 name1 = 'admin'
+name2 = 'varun'
 pas1 = 'test'
+pas2 ='varun'
 s2=name1+pas1
 deafulthash = sha512(s2 .encode()).hexdigest()
-print(deafulthash)
+print('\n Default Hash Value \n',deafulthash,'\n')
 
-if(name==name1 and pas == pas1):
-    print('Regular Validation Successful')
+if((name==name1 and pas == pas1) or (name==name2 and pas == pas2)):
+    print('Regular Validation Successful \n')
     if(newhash == deafulthash):
-        print('user authentication Successful')
+        print('User Authentication Successful \n')
     else:
-        print('user authentication unSuccessful')
+        print('!!User Authentication Failed!! \n')
 else:
-    print('Regular Validation Failed')
+    print('!!Regular Validation Failed!! \n')
